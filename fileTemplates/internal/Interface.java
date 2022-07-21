86c4283e433f4a45f6dd474729a106c0eacfd025
wuqia <254424864@qq.com>
2022-07-21 09:58:10 +0800

    IC-2022.1.3 <wuqia@wuqia-102278.local Create FeatureSuggester.xml, Record.java, laf.xml, ide.general.xml, SquaretestSettings.xml, Enum.java, Interface.java, Class.java, jdk.table.xml, find.xml, DontShowAgainFeedbackService.xml, filetypes.xml, settingsSyncPlugins.xml, vcs.xml, androidEditors.xml, editor-font.xml, colors.scheme.xml, KeyPromoterXSettings.xml, github.xml, debugger.xml, sqt_vm_cache.xml, sonarlint.xml, updates.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ *@Description : TODO
+ *@Author : ${USER}
+ *@Date : ${DATE} ${TIME}
+ *@Version : 1.0
+ **/
+public interface ${NAME} {
+}
